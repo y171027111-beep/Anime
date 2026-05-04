dd4f94696fdf0903b50205b38cd688998cbc61de
y171027111-beep <y171027111@gmail.com>
2026-05-04 18:45:53 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-BOT_TOKEN  = "8616053081:AAHJ1uIfXsGCzRQzMTXDwPPj5apz3EkqBys"
-CHANNEL_ID = "@anime_uzbekchao"
-ADMIN_IDS  = [8467353523,8348353169]
+import os
+
+BOT_TOKEN  = os.environ.get("8616053081:AAHJ1uIfXsGCzRQzMTXDwPPj5apz3EkqBys", "")
+CHANNEL_ID = os.environ.get("CHANNEL_ID", "@anime_uzbekchao")
+ADMIN_IDS  = [int(x) for x in os.environ.get("ADMIN_IDS", "8467353523,8348353169").split(",")]
